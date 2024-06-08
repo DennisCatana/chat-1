4e6425095275990668278c11429aaccd8d6d4077
DennisCatana <dennis.joel235@outlook.es>
2024-06-07 22:54:12 -0500

    apk lista

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyDOE7D6fWmRdj-AOZsryOjEcTKeWQylSs4",
-    authDomain: "proyecto-d01d0.firebaseapp.com",
-    databaseURL: "https://proyecto-d01d0-default-rtdb.firebaseio.com",
-    projectId: "proyecto-d01d0",
-    storageBucket: "proyecto-d01d0.appspot.com",
-    messagingSenderId: "53258364794",
-    appId: "1:53258364794:web:b663b4131c6b1d244618c3",
-    measurementId: "G-VGME2G21D0"
+    apiKey: "AIzaSyD9vtkKDUHGJxEIvVADv0miHPqRXS9wm3E",
+    authDomain: "chat2023-bae0a.firebaseapp.com",
+    databaseURL: "https://chat2023-bae0a-default-rtdb.firebaseio.com",
+    projectId: "chat2023-bae0a",
+    storageBucket: "chat2023-bae0a.appspot.com",
+    messagingSenderId: "578691690025",
+    appId: "1:578691690025:web:e4316e29d5faecddf9b638",
+    measurementId: "G-DPS7004V9M"
   }
   
 };
